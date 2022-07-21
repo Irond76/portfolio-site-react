0c6606e61d90e59c20f048adf5da6428e3ea751d
Dale Bendixen <dalebendixen@gmail.com>
2022-07-21 17:11:40 -0600

    created components folder and site enter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import SiteEnter from './components/SiteEnter';
+
 
 function App() {
   return (
     <div className="App">
+      <SiteEnter />
     <h2>Hello World</h2>
     </div>
       
---
+++ src/components/SiteEnter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SiteEnter = () => {
+  return (
+    <div>SiteEnter</div>
+  )
+}
+
+export default SiteEnter
