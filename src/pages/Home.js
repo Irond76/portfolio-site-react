a3cd6deea842137c674d8354b1b4f7eee6e1dfba
Dale Bendixen <dalebendixen@gmail.com>
2022-07-23 16:41:16 -0600

    fixed styling added about me text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,23 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className='img-container'>
-      <div className="header-image"></div>
-    </div>
+    <>
+        <div className='img-container'>
+          <div className="header-image"></div>
+        </div>
+        <div className="about">
+          <section className="about-section-title">
+              <h1>About me </h1>
+                <br/>
+                <p className="about-me-info">
+                  My name is <span style={{fontWeight: 'bold'}}>Dale Bendixen</span>, I'm an enthusiastic <span style={{fontWeight: 'bold'}}>Front-End Developer</span>. I was born and raised in beautiful Portland, Maine, before moving to charming Boise, Idaho; where I raised my three children and started my two businesses. My Property Maintenance company focused primairly on commercial properties in and around the Boise area. With Bendixen Photography I was able to cater more towards my creative side, providing a wide range of services such as: family and high-school graduation portraits, head shots for corporate accounts and real-estate photography. I enjoyed challenging myself learning Adobe Photoshop, Adobe Premier Pro, videography as well as astrophotography. I'm a self-taught musician, singer/song-writer playing the acoustic/electric guitar, piano and drums. Music is a great love of mine. I've writen, performed and produced my own album in my home studio. As an experienced business owner and seasoned professional I bring an array of knowledge to my <span style={{fontWeight: 'bold'}}>Front-End Developing</span> career. I have such a passion for the technologies industry that I closed down my successful business to dedicate all of my energy towards <span style={{fontWeight: 'bold'}}>Front-End Developing</span>.
+                </p>
+            </section>
+
+        </div>
+        
+    
+    </>
 
   )
 }
